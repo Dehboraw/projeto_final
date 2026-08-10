631d851fd1cdd36f8fa87b844bb466df8c8bc950
Dehboraw <alvezdebora2007@gmail.com>
2026-08-10 12:17:34 -0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,15 @@
+// arquivo para fazer conexão 
+// npm i mysql2
+const mysql = require("mysql2/promise")
+
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    port: 3306, 
+    user: 'root', 
+    password: 'escola',
+    database: 'SistemaCirurgia'
+})
+
+module.exports = Object.freeze({
+    pool: pool
+})
