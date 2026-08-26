9443c0767243374ca7f4b0f37c6992cd1f98702d
Dehboraw <alvezdebora2007@gmail.com>
2026-08-26 09:21:45 -0300

    Update cadastro.sql
    Banco completo

--- cadastro.sql
+++ cadastro.sql
@@ -1,13 +1,58 @@
-CREATE TABLE Cliente 
-( 
- id INT PRIMARY KEY AUTO_INCREMENT,  
- nome VARCHAR(50) NOT NULL,  
- cpf CHAR(14) NOT NULL UNIQUE, 
- celular CHAR(14) NOT NULL,  
- email VARCHAR(50) NOT NULL UNIQUE,   
- senha VARCHAR(512) NOT NULL
-); 
+/*BANCO NOVO*/
+CREATE TABLE Cliente
+(
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(50) NOT NULL, 
+cpf CHAR(14) NOT NULL UNIQUE, 
+celular CHAR(14) NOT NULL, 
+email VARCHAR(50) NOT NULL UNIQUE, 
+senha VARCHAR(512) NOT NULL 
+);
+CREATE TABLE Compra
+(
+id INT PRIMARY KEY AUTO_INCREMENT, 
+idEndereco INT NOT NULL, 
+idCliente INT NOT NULL, 
+data_da_compra DATE NOT NULL, 
+forma_pagamento VARCHAR(100) NOT NULL, 
+valor_total VARCHAR(50) NOT NULL
+);
+CREATE TABLE Produto
+(
+id INT PRIMARY KEY AUTO_INCREMENT, 
+nome VARCHAR(80) NOT NULL, 
+descricao VARCHAR(200) NOT NULL, 
+categoria VARCHAR(80) NOT NULL, 
+preco VARCHAR(50) NOT NULL, 
+caminho_img VARCHAR(200) DEFAULT '/fotos/semfoto.png', 
+quantidade_img INT DEFAULT '1'
+);
+CREATE TABLE Endereco
+(
+id INT PRIMARY KEY AUTO_INCREMENT, 
+idCliente INT NOT NULL, 
+estado VARCHAR(40) NOT NULL, 
+cidade VARCHAR(60) NOT NULL, 
+bairro VARCHAR(60) NOT NULL, 
+numero INT NOT NULL, 
+rua VARCHAR(180) NOT NULL
+);
+CREATE TABLE CompraProduto (
+idCompra INT,
+idProduto INT,
+quantidade INT NOT NULL,
+valor_unitario VARCHAR(50) NOT NULL,
+PRIMARY KEY (idCompra, idProduto),
+FOREIGN KEY (idCompra) REFERENCES Compra(id),
+FOREIGN KEY (idProduto) REFERENCES Produto(id)
+);
 
+ALTER TABLE Compra ADD FOREIGN KEY(idEndereco) REFERENCES Endereco(id);
+ALTER TABLE Compra ADD FOREIGN KEY(idCliente) REFERENCES Cliente(id);
+ALTER TABLE Endereco ADD FOREIGN KEY(idCliente) REFERENCES Cliente(id);
+ALTER TABLE CompraProduto ADD FOREIGN KEY(idCompra) REFERENCES Compra(id);
+ALTER TABLE CompraProduto ADD FOREIGN KEY(idProduto) REFERENCES Produto(id);
+ 
 INSERT INTO Cliente (nome, cpf, celular, email, senha) VALUES ("Débora", "111.222.333-00", "(42)99999-0909", "deh@gmail.com", "dy2905")
 
 INSERT INTO Cliente (nome, cpf, celular, email, senha) VALUES
@@ -22,7 +67,7 @@ INSERT INTO Cliente (nome, cpf, celular, email, senha) VALUES
 ('Isabela Martins', '999.000.111-22', '(91)90987-6543', 'isabela.martins@email.com', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
 ('João Pereira', '000.111.222-33', '(85)99876-5432', 'joao.pereira@email.com', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8');
 
-SELECT email, senha FROM Cliente;
+SELECT email, senha FROM Cliente WHERE cpf = "222.333.444-55";
 
 SELECT email, senha FROM Cliente WHERE email = "ana.silva@email.com";
 
@@ -33,4 +78,3 @@ SELECT * FROM Cliente WHERE id <= 10 AND LENGTH(senha) > 20;
 DELETE FROM Cliente WHERE id = 11;
 
 UPDATE Cliente SET nome = "Débora Alves", email = "Alves@gmail.com" WHERE id = 1;
-
